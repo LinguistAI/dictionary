f4f692fa749e278dfebfdb3b119466e2deedf97d
Ilkim Elif Kervan <elif.kervan.1907@gmail.com>
2024-02-04 02:37:03 +0300

    Seperated marriem webster configs from dictionary service implementation

--- src/service/dictionary-service.ts
+++ src/service/dictionary-service.ts
@@ -1,50 +1,34 @@
 import { APIRequestExc, WordNotFoundExc } from "../common/exception";
 import config from "../config/config";
 import axios from "axios";
-import { create_audio_url } from "../utils/audio-file-utils";
 import { DictionaryResponse } from "../model/dictionary-response";
 import { DictionaryWordGroup, WordDef } from "../model/dictionary-word-group";
+import { create_audio_url } from "../utils/dictionary/audio-file-utils";
+import { APIConfig } from "../utils/dictionary/api-config";
+import { MarriemWebsterConfig } from "../utils/dictionary/config-merriam-webster";
 
 export class DictionaryService {
     search_word(wordList: string[]): Promise<any> {
         const promises = wordList.map((word: string) => {
             return new Promise<DictionaryResponse>((resolve, reject) => {
-                const axios_config = {
-                    method: "get",
-                    url: config.DICTIONARY_API_URL + word,
-                    params: {
-                        key: config.DICTIONARY_API_KEY,
-                        format: "json",
-                        jscmd: "data",
-                    },
-                };
+                const apiConfig = this.select_api();
 
-                axios(axios_config)
+                axios(apiConfig.axiosConfig(word))
                     .then((res) => {
-                        if (res.status != 200 || typeof res.data === "string") {
-                            reject(new APIRequestExc(res.data));
-                        } else if (
-                            Object.keys(res.data).length === 0 ||
-                            (Array.isArray(res.data) &&
-                                !res.data.every(
-                                    (item) => typeof item === "object"
-                                ))
-                        ) {
-                            const result: DictionaryResponse = {
-                                [word]: {},
-                            };
-                            resolve(result);
+                        apiConfig.handleErrors(res);
+                        const edgeCaseResult: DictionaryResponse | null =
+                            apiConfig.handleEdgeCases(res, word);
+
+                        if (edgeCaseResult) {
+                            resolve(edgeCaseResult);
                         } else {
-                            console.log("status: ", res.status);
-                            console.log("data: ", res.data);
                             const dict_response = JSON.parse(
                                 JSON.stringify(res.data)
                             );
-                            console.log("data: ", res.data);
                             const wordGroup: DictionaryWordGroup[] =
                                 dict_response
                                     .map((entry: any) =>
-                                        this.extract_dict_data(word, entry)
+                                        apiConfig.extractDictData(word, entry)
                                     )
                                     .filter(
                                         (result: DictionaryWordGroup | null) =>
@@ -65,6 +49,7 @@ export class DictionaryService {
             });
         });
 
+        // reshape response by removing array structure
         return Promise.all(promises).then((data) => {
             return data.reduce((wordObj: Record<string, any>, item: any) => {
                 const key = Object.keys(item)[0];
@@ -74,48 +59,10 @@ export class DictionaryService {
         });
     }
 
-    private extract_dict_data(
-        word: string,
-        jsonEntry: any
-    ): DictionaryWordGroup | undefined {
-        const { meta, def, hwi, fl } = jsonEntry;
-        const idPattern: RegExp = new RegExp(`^[${word}:0-9]+$`);
+    private select_api(): APIConfig {
+        let selectedAPI: APIConfig;
 
-        // if definitions are not of the exact search word, do not process them
-        if (!(idPattern.test(meta.id) || word.trim() == meta.id)) {
-            return undefined;
-        }
-
-        // extract the definitions and example usages
-        const definitionArray: WordDef[] = def?.[0]?.sseq?.map(
-            this.extract_def
-        );
-
-        console.log("audio: ", hwi?.prs?.[0]?.sound?.audio);
-
-        // extract meta id, audio file name, and functional label
-        const result: DictionaryWordGroup = {
-            id: meta.id,
-            word: word,
-            audio: hwi?.prs?.[0]?.sound?.audio
-                ? create_audio_url(hwi?.prs?.[0]?.sound?.audio)
-                : "",
-            phonetic: hwi?.prs?.[0]?.mw || "",
-            func_label: fl || "",
-            meaning: definitionArray,
-        };
-
-        return result;
-    }
-
-    private extract_def(sseq: any): WordDef {
-        const definitionObj: WordDef = {
-            definition: sseq?.[0]?.[1]?.dt?.[0]?.[1] || "",
-            examples:
-                sseq[0]?.[1]?.dt?.[1]?.[1]?.map((def: { t: any }) => def.t) ||
-                [],
-        };
-
-        return definitionObj;
+        selectedAPI = MarriemWebsterConfig;
+        return selectedAPI;
     }
 }
---
+++ src/utils/dictionary/api-config.ts
@@ -0,0 +1,13 @@
+import config from "../../config/config";
+import { DictionaryResponse } from "../../model/dictionary-response";
+import { DictionaryWordGroup } from "../../model/dictionary-word-group";
+
+export interface APIConfig {
+    axiosConfig: (word: string) => any;
+    extractDictData: (
+        word: string,
+        data: any
+    ) => DictionaryWordGroup | undefined;
+    handleErrors: (res: any) => any;
+    handleEdgeCases: (res: any, word: string) => DictionaryResponse | null;
+}
