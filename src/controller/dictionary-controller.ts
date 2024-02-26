b423b1dadeb56b1e89f60b1c3ae5f179fd44b1e7
Tolga Özgün <tolgaozgunn@gmail.com>
2024-02-26 19:35:24 +0300

    Add ping endpoint

--- src/controller/dictionary-controller.ts
+++ src/controller/dictionary-controller.ts
@@ -1,7 +1,6 @@
-import { Router, Request, Response, NextFunction } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { Exception, ValidationExc } from "../common/exception";
 import { ResponseSuccess } from "../common/response-success";
-import { WordSearchParams } from "../common/search-word-params";
 import { LogStatus } from "../enums/log-status";
 import { DictionaryService } from "../service/dictionary-service";
 import log_service from "../service/log-service";
@@ -9,54 +8,51 @@ import wordValidation from "../validation/library-validation";
 import BaseRouter from "./base-router";
 
 class DictionaryController implements BaseRouter {
-    router: Router;
-    dictionaryService: DictionaryService;
+  router: Router;
+  dictionaryService: DictionaryService;
 
-    constructor() {
-        this.dictionaryService = new DictionaryService();
-        this.router = Router();
-        this.init_controller();
-    }
+  constructor() {
+    this.dictionaryService = new DictionaryService();
+    this.router = Router();
+    this.init_controller();
+  }
 
-    init_controller() {
-        this.router.post("/", this.search_word);
-    }
+  init_controller() {
+    this.router.post("/", this.search_word);
+    this.router.get("/ping", this.ping);
+  }
 
-    private search_word = async (
-        req: Request,
-        res: Response,
-        next: NextFunction
-    ) => {
-        const wordList = req.body.wordList;
+  private search_word = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const wordList = req.body.wordList;
 
-        wordValidation.searchSchema
-            .validateAsync(wordList)
-            .then((validated) => {
-                this.dictionaryService
-                    .search_word(validated)
-                    .then((dict) => {
-                        log_service.log(
-                            LogStatus.Success,
-                            `search word: ${validated}`
-                        );
-                        return res.json(
-                            new ResponseSuccess("ok", { dict: dict })
-                        );
-                    })
-                    .catch((err) => {
-                        log_service.log(
-                            LogStatus.Error,
-                            `search word: ${validated}`
-                        );
-                        next(err);
-                    });
-            })
-            .catch((err) => {
-                log_service.log(LogStatus.Error, `search words: ${wordList}`);
-                const exc: Exception = new ValidationExc(err);
-                next(exc);
-            });
-    };
+    wordValidation.searchSchema
+      .validateAsync(wordList)
+      .then((validated) => {
+        this.dictionaryService
+          .search_word(validated)
+          .then((dict) => {
+            log_service.log(LogStatus.Success, `search word: ${validated}`);
+            return res.json(new ResponseSuccess("ok", { dict: dict }));
+          })
+          .catch((err) => {
+            log_service.log(LogStatus.Error, `search word: ${validated}`);
+            next(err);
+          });
+      })
+      .catch((err) => {
+        log_service.log(LogStatus.Error, `search words: ${wordList}`);
+        const exc: Exception = new ValidationExc(err);
+        next(exc);
+      });
+  };
+
+  private ping = (res: Response) => {
+    res.json(new ResponseSuccess("ok", { message: "Server is up" }));
+  };
 }
 
 const dictionary_controller = new DictionaryController();
